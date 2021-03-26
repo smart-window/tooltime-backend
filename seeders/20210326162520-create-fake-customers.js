a6f7d5af4ec7f6d4e3de528e47d976df4e02ee26
smart-window <smart.free.dev@gmail.com>
2021-03-26 09:46:36 -0700

    Adding Fake Customer Seeder

---
+++ seeders/20210326162520-create-fake-customers.js
@@ -0,0 +1,26 @@
+'use strict';
+const bcrypt = require('bcrypt')
+const uuid = require('uuid');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Customers',
+      [
+        {
+          id: uuid.v4(),
+          name: 'Default User',
+          email: 'admin@admin.com',
+          password: bcrypt.hashSync('aaa', bcrypt.genSaltSync(8)),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      ],
+      {},
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('Customers', null, {});
+  }
+};
