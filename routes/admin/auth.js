66330dd66c75f308feed60b4e2f700d0ba8f067a
smart-window <smart.free.dev@gmail.com>
2021-05-04 08:15:05 -0500

    fix admin auth routing

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -12,32 +12,34 @@ const connectToDatabase = require('../../database/index')
  */
 router.post('/login', async (req, res) => {
   console.log('[POST] /admin/auth/login =>', req.body)
-  const { email, password } = req.body
-  const { User } = await connectToDatabase()
-  const loggedUser = await User.findOne({ where: { email: email } })
-
-  if (!loggedUser) {
-    res.status(StatusCodes.UNAUTHORIZED).send('Incorrect Credentials')
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      res.status(StatusCodes.BAD_REQUEST).json({ message: 'Bad request' })
+      return
+    }
+    const { User } = await connectToDatabase()
+    const loggedUser = await User.findOne({ where: { email: email } })
+    if (!loggedUser) {
+      res.status(StatusCodes.UNAUTHORIZED).send('Incorrect Credentials')
+      return
+    }
+    const isPwdMatch = await bcrypt.compare(password, loggedUser.password())
+    if (!isPwdMatch) {
+      res.status(StatusCodes.UNAUTHORIZED).send('Incorrect Credentials')
+      return
+    }
+    const payload = {
+      email: loggedUser.email,
+    }
+    const jwtToken = jwt.sign(payload, process.env.AUTH_TOKEN_SECRET, {
+      expiresIn: '1d',
+    })
+    res.send({ accessToken: jwtToken })
     return
+  } catch (e) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: e.message })
   }
-
-  const isPwdMatch = await bcrypt.compare(password, loggedUser.password())
-
-  if (!isPwdMatch) {
-    res.status(StatusCodes.UNAUTHORIZED).send('Incorrect Credentials')
-    return
-  }
-
-  const payload = {
-    email: loggedUser.email,
-  }
-
-  const jwtToken = jwt.sign(payload, process.env.AUTH_TOKEN_SECRET, {
-    expiresIn: '1d',
-  })
-
-  res.send({ accessToken: jwtToken })
-  return
 })
 
 module.exports = router
