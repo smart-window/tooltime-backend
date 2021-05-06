22ad1e2df353ac7ed0c96a556408599d5e215409
smart-window <smart.free.dev@gmail.com>
2021-05-06 08:08:52 -0500

    update fake user generating seeder

--- seeders/20210430142101-create-fake-users.js
+++ seeders/20210430142101-create-fake-users.js
@@ -18,7 +18,18 @@ module.exports = {
         {
           username: 'admin',
           email: 'admin@tt.com',
+          name: 'Admin Test Account',
           password: bcrypt.hashSync('aaa', bcrypt.genSaltSync(8)),
+          role: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: 'agent',
+          email: 'agent@tt.com',
+          name: 'Agent Test Account',
+          password: bcrypt.hashSync('aaa', bcrypt.genSaltSync(8)),
+          role: 'agent',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
