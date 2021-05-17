8fce290648bf8c7f6e8c47abe91ba466abfea236
smart-window <smart.free.dev@gmail.com>
2021-05-16 19:47:03 -0500

    update location migration

--- migrations/20210321134323-create-location.js
+++ migrations/20210321134323-create-location.js
@@ -1,30 +1,60 @@
 'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, DataTypes) => {
     await queryInterface.createTable('locations', {
       id: {
-        type: Sequelize.UUID,
+        type: DataTypes.UUID,
         primaryKey: true,
-        defaultValue: Sequelize.UUIDV4,
+        defaultValue: DataTypes.UUIDV4,
       },
-      name: Sequelize.STRING,
-      description: Sequelize.TEXT,
-      address_1: Sequelize.STRING,
-      address_2: Sequelize.STRING,
-      city: Sequelize.STRING,
-      state: Sequelize.STRING,
-      zip: Sequelize.STRING,
-      phone: Sequelize.STRING,
-      hours: Sequelize.STRING,
-      active: Sequelize.BOOLEAN,
+      name: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        notNull: { msg: 'location should have a name' },
+        notEmpty: { msg: 'location name must not be empty' },
+      },
+      description: DataTypes.TEXT,
+      address_1: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        notNull: { msg: 'location should have a address' },
+        notEmpty: { msg: 'address must not be empty' },
+      },
+      address_2: DataTypes.STRING,
+      city: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        notNull: { msg: 'location should have a city' },
+        notEmpty: { msg: 'city must not be empty' },
+      },
+      state: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        notNull: { msg: 'location should have a state' },
+        notEmpty: { msg: 'state must not be empty' },
+      },
+      zip: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        notNull: { msg: 'location should have a zip' },
+        notEmpty: { msg: 'zip must not be empty' },
+      },
+      phone: DataTypes.STRING,
+      hours: DataTypes.STRING,
+      active: DataTypes.BOOLEAN,
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
       },
     })
   },
