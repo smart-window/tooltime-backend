65d8ceec76189533d6092c800d901f86d2446a35
smart-window <smart.free.dev@gmail.com>
2021-06-06 12:17:06 +0430

    add orderItemId to assets

--- migrations/20210606074147-add-orderItemId-to-assets.js
+++ migrations/20210606074147-add-orderItemId-to-assets.js
@@ -1,21 +1,18 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn('assets', 'orderItemId', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+    ]);
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: (queryInterface) => {
+    return Promise.all([
+      queryInterface.removeColumn('assets', 'orderItemId'),
+    ]);
+  },
 };
