61a02eba4808494b73a2c5661831a989de0f9844
smart-window <smart.free.dev@gmail.com>
2021-06-12 06:07:11 +0430

    get delivery rate

--- routes/admin/order.js
+++ routes/admin/order.js
@@ -7,7 +7,6 @@ const { StatusCodes } = require('http-status-codes')
 
 router.post('/get_order_count', async (req, res) => {
   console.log('[GET] /order/get_order_count =>', req.params)
-  console.log('1234567890987654321234567890987654321234567890987654321', req.body)
   try {
     const { Order, OrderItem, Customer, Location, Product, Asset } = await connectToDatabase()
     // const list = []
@@ -18,7 +17,27 @@ router.post('/get_order_count', async (req, res) => {
       const list = await Order.sequelize.query("SELECT month_tbl.str AS selected_date, COUNT(orders.id) AS order_num FROM (SELECT DATE_FORMAT(m1, '%b') AS str, DATE_FORMAT(m1, '%Y-%m') AS MONTH FROM ( SELECT (CONCAT(YEAR(NOW()), '-01-01') - INTERVAL DAYOFMONTH( CONCAT(YEAR(NOW()), '-01-01') )-1 DAY) +INTERVAL m MONTH AS m1 FROM ( SELECT @rownum:=@rownum+1 AS m FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t1, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t2, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t3, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t4, (SELECT @rownum:=-1) t0 ) d1 ) d2 WHERE m1<=CONCAT(YEAR(NOW()), '-12-31') ORDER BY m1) AS month_tbl LEFT JOIN orders ON month_tbl.month = DATE_FORMAT(DATE(createdAt), '%Y-%m') GROUP BY month_tbl.month")
       res.send(list)
     } else if (req.body.type == "year") {
-      const list = await Order.sequelize.query("SELECT date_tbl.selected_date, COUNT(orders.id) AS order_num FROM (SELECT * FROM (SELECT ADDDATE('1970-01-01', t4.i * 10000 + t3.i * 1000 + t2.i * 100 + t1.i * 10 + t0.i) selected_date FROM (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t0, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t1, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t2, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t3, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t4) v WHERE selected_date BETWEEN(SELECT DATE(NOW() + INTERVAL(1 - DAYOFWEEK(NOW())) DAY)) AND(SELECT DATE(NOW() + INTERVAL(7 - DAYOFWEEK(NOW())) DAY))) AS date_tbl LEFT JOIN orders ON date_tbl.selected_date = DATE(orders.createdAt) GROUP BY date_tbl.selected_date")
+      const list = await Order.sequelize.query("SELECT year_tbl.year AS selected_date, COUNT(orders.id) AS order_num FROM (SELECT YEAR(NOW()) AS YEAR UNION SELECT YEAR(NOW()) - 1 UNION SELECT YEAR(NOW()) - 2 UNION SELECT YEAR(NOW()) - 3 UNION SELECT YEAR(NOW()) - 4 UNION SELECT YEAR(NOW()) - 5 UNION SELECT YEAR(NOW()) - 6 UNION SELECT YEAR(NOW()) - 7 UNION SELECT YEAR(NOW()) - 8 UNION SELECT YEAR(NOW()) - 9) AS year_tbl LEFT JOIN orders ON year_tbl.year = DATE_FORMAT(DATE(createdAt), '%Y') GROUP BY year_tbl.year")
+      res.send(list)
+    }
+  } catch (e) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: e.message })
+  }
+})
+
+router.post('/get_delivery_count', async (req, res) => {
+  console.log('[GET] /order/get_delivery_count =>', req.params)
+  try {
+    const { Order, OrderItem, Customer, Location, Product, Asset } = await connectToDatabase()
+    // const list = []
+    if (req.body.type == "week") {
+      const list = await Order.sequelize.query("SELECT date_tbl.selected_date, COUNT(orders.id) AS order_num FROM (SELECT * FROM (SELECT ADDDATE('1970-01-01', t4.i * 10000 + t3.i * 1000 + t2.i * 100 + t1.i * 10 + t0.i) selected_date FROM (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t0, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t1, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t2, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t3, (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t4) v WHERE selected_date BETWEEN(SELECT DATE(NOW() + INTERVAL(1 - DAYOFWEEK(NOW())) DAY)) AND(SELECT DATE(NOW() + INTERVAL(7 - DAYOFWEEK(NOW())) DAY))) AS date_tbl LEFT JOIN orders ON date_tbl.selected_date = DATE(orders.createdAt) AND (orders.status != 'PICKED' AND orders.status != 'PENDING') GROUP BY date_tbl.selected_date")
+      res.send(list)
+    } else if (req.body.type == "month") {
+      const list = await Order.sequelize.query("SELECT month_tbl.str AS selected_date, COUNT(orders.id) AS order_num FROM (SELECT DATE_FORMAT(m1, '%b') AS str, DATE_FORMAT(m1, '%Y-%m') AS MONTH FROM ( SELECT (CONCAT(YEAR(NOW()), '-01-01') - INTERVAL DAYOFMONTH( CONCAT(YEAR(NOW()), '-01-01') )-1 DAY) +INTERVAL m MONTH AS m1 FROM ( SELECT @rownum:=@rownum+1 AS m FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t1, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t2, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t3, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t4, (SELECT @rownum:=-1) t0 ) d1 ) d2 WHERE m1<=CONCAT(YEAR(NOW()), '-12-31') ORDER BY m1) AS month_tbl LEFT JOIN orders ON month_tbl.month = DATE_FORMAT(DATE(createdAt), '%Y-%m') AND (orders.status != 'PICKED' AND orders.status != 'PENDING') GROUP BY month_tbl.month")
+      res.send(list)
+    } else if (req.body.type == "year") {
+      const list = await Order.sequelize.query("SELECT year_tbl.year AS selected_date, COUNT(orders.id) AS order_num FROM (SELECT YEAR(NOW()) AS YEAR UNION SELECT YEAR(NOW()) - 1 UNION SELECT YEAR(NOW()) - 2 UNION SELECT YEAR(NOW()) - 3 UNION SELECT YEAR(NOW()) - 4 UNION SELECT YEAR(NOW()) - 5 UNION SELECT YEAR(NOW()) - 6 UNION SELECT YEAR(NOW()) - 7 UNION SELECT YEAR(NOW()) - 8 UNION SELECT YEAR(NOW()) - 9) AS year_tbl LEFT JOIN orders ON year_tbl.year = DATE_FORMAT(DATE(createdAt), '%Y') AND (orders.status != 'PICKED' AND orders.status != 'PENDING') GROUP BY year_tbl.year")
       res.send(list)
     }
   } catch (e) {
