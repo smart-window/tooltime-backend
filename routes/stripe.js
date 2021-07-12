0603e6f348e7059f0dee7983aff661153a21899b
smart-window <smart.free.dev@gmail.com>
2021-07-12 08:22:24 -0500

    update env.dist

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,15 +6,15 @@ var cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // Fetch the Checkout Session to display the JSON result on the success page
-router.get("/checkout-session", async (req, res) => {
-  const { sessionId } = req.query;
-  const session = await stripe.checkout.sessions.retrieve(sessionId);
-  res.send(session);
-});
+router.get('/checkout-session', async (req, res) => {
+  const { sessionId } = req.query
+  const session = await stripe.checkout.sessions.retrieve(sessionId)
+  res.send(session)
+})
 
-router.post("/create-checkout-session", async (req, res) => {
-  const domainURL = process.env.DOMAIN;
-  const { priceId } = req.body;
+router.post('/create-checkout-session', async (req, res) => {
+  const domainURL = process.env.DOMAIN
+  const { priceId } = req.body
   // Create new Checkout Session for the order
   // Other optional params include:
   // [billing_address_collection] - to display billing address details on the page
@@ -23,8 +23,8 @@ router.post("/create-checkout-session", async (req, res) => {
   // For full details see https://stripe.com/docs/api/checkout/sessions/create
   try {
     const session = await stripe.checkout.sessions.create({
-      mode: "subscription",
-      payment_method_types: ["card"],
+      mode: 'subscription',
+      payment_method_types: ['card'],
       line_items: [
         {
           // price: priceId,
@@ -37,83 +37,82 @@ router.post("/create-checkout-session", async (req, res) => {
       success_url: `${domainURL}/billing-plan?session_id={CHECKOUT_SESSION_ID}`,
       // cancel_url: `${domainURL}/canceled.html`,
       cancel_url: `${domainURL}/billing-plan`,
-    });
+    })
 
-    return res.redirect(303, session.url);
+    return res.redirect(303, session.url)
   } catch (e) {
-    res.status(400);
+    res.status(400)
     return res.send({
       error: {
         message: e.message,
-      }
-    });
+      },
+    })
   }
-});
+})
 
-router.get("/config", (req, res) => {
+router.get('/config', (req, res) => {
   res.send({
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     basicPrice: process.env.BASIC_PRICE_ID,
     proPrice: process.env.PRO_PRICE_ID,
-  });
-});
+  })
+})
 
 router.post('/customer-portal', async (req, res) => {
   // For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
   // Typically this is stored alongside the authenticated user in your database.
-  const { sessionId } = req.body;
-  const checkoutsession = await stripe.checkout.sessions.retrieve(sessionId);
+  const { sessionId } = req.body
+  const checkoutsession = await stripe.checkout.sessions.retrieve(sessionId)
 
   // This is the url to which the customer will be redirected when they are done
   // managing their billing with the portal.
-  const returnUrl = process.env.DOMAIN;
+  const returnUrl = process.env.DOMAIN
 
   const portalsession = await stripe.billingPortal.sessions.create({
     customer: checkoutsession.customer,
     return_url: returnUrl,
-  });
+  })
 
   res.send({
     url: portalsession.url,
-  });
-});
+  })
+})
 
 // Webhook handler for asynchronous events.
-router.post("/webhook", async (req, res) => {
-  let data;
-  let eventType;
+router.post('/webhook', async (req, res) => {
+  let data
+  let eventType
   // Check if webhook signing is configured.
   if (process.env.STRIPE_WEBHOOK_SECRET) {
     // Retrieve the event by verifying the signature using the raw body and secret.
-    let event;
-    let signature = req.headers["stripe-signature"];
+    let event
+    let signature = req.headers['stripe-signature']
 
     try {
       event = stripe.webhooks.constructEvent(
         req.rawBody,
         signature,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
+        process.env.STRIPE_WEBHOOK_SECRET,
+      )
     } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`);
-      return res.sendStatus(400);
+      console.log(`⚠️  Webhook signature verification failed.`)
+      return res.sendStatus(400)
     }
     // Extract the object from the event.
-    data = event.data;
-    eventType = event.type;
+    data = event.data
+    eventType = event.type
   } else {
     // Webhook signing is recommended, but if the secret is not configured in `config.js`,
     // retrieve the event data directly from the request body.
-    data = req.body.data;
-    eventType = req.body.type;
+    data = req.body.data
+    eventType = req.body.type
   }
 
-  if (eventType === "checkout.session.completed") {
-    console.log(`🔔  Payment received!`);
+  if (eventType === 'checkout.session.completed') {
+    console.log(`🔔  Payment received!`)
   }
 
-  res.sendStatus(200);
-});
-
+  res.sendStatus(200)
+})
 
 module.exports = router
