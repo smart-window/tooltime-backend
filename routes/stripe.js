e1ea5bf4464572d81f8f254c3894447a82f1feca
smart-window <smart.free.dev@gmail.com>
2021-07-16 21:30:05 +0400

    update stripe redirect route

--- routes/stripe.js
+++ routes/stripe.js
@@ -15,7 +15,7 @@ router.get('/checkout-session', async (req, res) => {
 
 router.post('/create-checkout-session', async (req, res) => {
   const domainURL = process.env.DOMAIN
-  const { priceId } = req.body
+  const { priceId, routeName } = req.body
   // Create new Checkout Session for the order
   // Other optional params include:
   // [billing_address_collection] - to display billing address details on the page
@@ -33,8 +33,8 @@ router.post('/create-checkout-session', async (req, res) => {
         },
       ],
       // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-      success_url: `${domainURL}/billing-plan?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${domainURL}/billing-plan`,
+      success_url: `${domainURL}${routeName}?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${domainURL}${routeName}`,
     })
 
     return res.redirect(303, session.url)
