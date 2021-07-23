948fe6f94a7fba874671288629bde6afa5a36332
smart-window <smart.free.dev@gmail.com>
2021-07-24 00:02:57 +0430

    update email image

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -16,9 +16,10 @@ module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
         from: user,
         to: email,
         subject: "Please confirm your account",
-        html: `<h1>Email Confirmation</h1>
+        html: `<h1>Welcome to TOOLTIME</h1>
+          <img style='width:100%' src='https://www.tennantco.com/content/dam/tennant/tennantco/Campaigns/2017/17-na-all-all-stone-care-products/stone-care-banner.jpg'/>
           <h2>Hello ${name}</h2>
-          <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+          <p>Please confirm your email by clicking on the following link</p>
           <a href=${process.env.DOMAIN}/auth/confirm/${confirmationCode}> Click here</a>
           </div>`,
     }).catch(err => console.log(err));
